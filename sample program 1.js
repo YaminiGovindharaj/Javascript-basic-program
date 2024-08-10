2c42d820fdd43c4f41c5037b131168d889001154
YaminiGovindharaj <155999350+YaminiGovindharaj@users.noreply.github.com>
2024-08-10 22:49:31 +0530

    Update sample program 1.js

--- sample program 1.js
+++ sample program 1.js
@@ -2,7 +2,7 @@ const stringArr = ["a", "b", "c","n","j", "u","k"];
 var count=0;
 for(var i=0;i<stringArr.length;i++)
 {
-if(stringArr[i]=="a" || stringArr[i]=="e" || stringArr[i] → =="i" || stringArr[i]=="o" || stringArr[i]=="u"){
+if(stringArr[i]=="a" || stringArr[i]=="e" || stringArr[i] =="i" || stringArr[i]=="o" || stringArr[i]=="u"){
 count = count+1;
 }
 }
