dd723570d4c3d53f5ff39c0359a576a9a152359c
YaminiGovindharaj <155999350+YaminiGovindharaj@users.noreply.github.com>
2024-08-10 22:44:04 +0530

    Create sample program 1.js

---
+++ sample program 1.js
@@ -0,0 +1,9 @@
+const stringArr = ["a", "b", "c","n","j", "u","k"];
+var count=0;
+for(var i=0;i<stringArr.length;i++)
+{
+if(stringArr[i]=="a" || stringArr[i]=="e" || stringArr[i] → =="i" || stringArr[i]=="o" || stringArr[i]=="u"){
+count = count+1;
+}
+}
+console.log(count)
