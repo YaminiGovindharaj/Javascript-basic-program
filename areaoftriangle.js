eaab3f814fe96c2e53f96bc790ba1219ffa29ed2
YaminiGovindharaj <155999350+YaminiGovindharaj@users.noreply.github.com>
2024-08-17 18:43:13 +0530

    Create areaoftriangle.js

---
+++ areaoftriangle.js
@@ -0,0 +1,9 @@
+function triangleArea(base, height)
+{
+  return 0.5 * base * height;
+}
+
+let baseLength = 10;
+let heightLength = 5;
+let area = triangleArea(baseLength, heightLength);
+console.log(`The area of the triangle is: ${area}`);
