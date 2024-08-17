f72e4d149d37c306083a7033e5678e480a8aa2f7
YaminiGovindharaj <155999350+YaminiGovindharaj@users.noreply.github.com>
2024-08-17 18:49:05 +0530

    Create areaofcircle.js

---
+++ areaofcircle.js
@@ -0,0 +1,9 @@
+let pi = 3.14;
+    function findArea(r) 
+    {
+        return (pi * r * r);
+    }
+    let r, Area;
+    r = 10;
+    Area = findArea(r);
+    console.log("Area of Circle is: " + Area);
